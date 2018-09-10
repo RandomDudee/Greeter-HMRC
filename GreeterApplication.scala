01a6fbe523f194e6cc061d895da04288d215e67f
Sean <sean.rickards@digital.hmrc.gov.uk>
2018-09-10 17:06:42 +0100

    test

--- GreeterApplication.scala
+++ GreeterApplication.scala
@@ -6,9 +6,15 @@ import scala.io.StdIn
 object GreeterApplication extends App{
 
 
-  def greet(name: String) = println("Hello " + name )
+  def greet(name: String): Unit = {
+    if (name == "adam"){
+      println("You don't get a hello!")
+    } else {
+      println("Hello " + name)
+    }
+  }
 
-  val name = StdIn.readLine("What is your name?")
+  val name = StdIn.readLine("What is your name? ")
 
   greet(name)
 
